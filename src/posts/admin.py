0db415486a33dbd9f639141a29a2911bf06a5ecb
evhalp <evhalp@gmail.com>
2025-10-11 14:37:18 -0400

    Add Post Models (database)

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'description_preview', 'location', 'start_time', 'created_at']
+    list_filter = ['created_at', 'start_time']
+    search_fields = ['title', 'description', 'location', 'user__username']
+
+    def description_preview(self, obj):
+        return f'{obj.description[:50]}...' if len(obj.description) > 50 else obj.description
+    description_preview.short_description = 'Description'
--- src/posts/models.py
+++ src/posts/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255, blank=True, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    image_path = models.CharField(max_length=500, blank=True, null=True)
+    # Django could also handle image uploads with models.ImageField(upload_to='posts/', blank=True, null=True), if we prefer
+    # We'd need to pip install pillow, first, though
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'"{self.title}" by {self.user.username}'
