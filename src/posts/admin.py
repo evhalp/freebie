42473302738c1e45e3a44739f645da2f5bdcca5b
evhalp <evhalp@gmail.com>
2025-10-11 14:39:19 -0400

    Add Comment Models (database)

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import Tag, Post
+from .models import Tag, Post, Comment
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -25,3 +25,14 @@ class PostAdmin(admin.ModelAdmin):
     def description_preview(self, obj):
         return f'{obj.description[:50]}...' if len(obj.description) > 50 else obj.description
     description_preview.short_description = 'Description'
+
+@admin.register(Comment)
+class PostComment(admin.ModelAdmin):
+    list_display = ['post', 'user', 'content_preview', 'parent', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['content', 'user__username', 'post__title']
+    raw_id_fields = ['post', 'user', 'parent']
+
+    def content_preview(self, obj):
+        return f'{obj.content[:50]}...' if len(obj.content) > 50 else obj.content
+    content_preview.short_description = 'Content'
--- src/posts/models.py
+++ src/posts/models.py
@@ -37,3 +37,31 @@ class Post(models.Model):
 
     def __str__(self):
         return f'"{self.title}" by {self.user.username}'
+    
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    content = models.TextField()
+    parent = models.ForeignKey(  # For nested comments (replies to other comments)
+        'self',
+        on_delete=models.CASCADE,
+        related_name='replies',
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on "{self.post.title}"'
