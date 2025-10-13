d65f099b86266bd1a8d5de59b634032c89f4084e
evhalp <evanhalperin@ufl.edu>
2025-10-13 13:24:06 -0400

    Add bio field to UserProfile

--- src/users/admin.py
+++ src/users/admin.py
@@ -4,5 +4,9 @@ from .models import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user__username', 'credibility']
+    list_display = ['user__username', 'bio_preview', 'credibility']
     search_fields = ['user__username']
+
+    def bio_preview(self, obj):
+        return f'{obj.bio[:50]}...' if len(obj.bio) > 50 else obj.bio
+    bio_preview.short_description = 'Biography'
--- src/users/models.py
+++ src/users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
     credibility = models.IntegerField(default=0)
 
     def __str__(self):
