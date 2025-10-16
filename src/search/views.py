1db457f651c33b9fb68730dc2074af4660246a90
KaiS987 <kaisteckley@gmail.com>
2025-10-16 11:32:35 -0400

    Add simple title/description query

--- src/search/views.py
+++ src/search/views.py
@@ -2,20 +2,28 @@
 
 # search/views.py
 from django.shortcuts import render
+from posts.models import Post
+from django.db.models import Q
 
-# pretend we have a database of items
-ITEMS = [
-    {"name": "Apple", "description": "A red fruit"},
-    {"name": "Banana", "description": "A yellow fruit"},
-    {"name": "Carrot", "description": "An orange vegetable"},
-]
 
 def search_view(request):
     """Display the search form."""
-    return render(request, 'search/search.html')
+    query = request.GET.get('q')
+    results = []
 
-def results_view(request):
-    """Show results based on search query."""
-    query = request.GET.get('q', '')  # get ?q= from URL
-    results = [item for item in ITEMS if query.lower() in item["name"].lower()] if query else []
-    return render(request, 'search/results.html', {"query": query, "results": results})
+    if query:
+        results = Post.objects.filter(Q(title__icontains=query) | Q(description__icontains=query)).distinct
+
+    else:
+        results = Post.objects.all()
+    
+    print("TEST")
+
+    context = {
+        'query': query,
+        'results': results
+    }
+
+
+    return render(request, 'search/search.html', context)
+
