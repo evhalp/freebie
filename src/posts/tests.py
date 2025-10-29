63ae2301d1fcfd5deaf162ab28bb41b6374b0200
evhalp <evhalp@gmail.com>
2025-10-29 15:09:46 -0400

    Edit imports

--- src/posts/tests.py
+++ src/posts/tests.py
@@ -1,5 +1,4 @@
-from django.test import TestCase, Client
-from django.urls import reverse
+from django.test import TestCase
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db import IntegrityError
