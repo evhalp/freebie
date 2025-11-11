7b554f7c109c67628f83b14e1c25b607d5e630ee
evhalp <evhalp@gmail.com>
2025-11-11 12:20:31 -0500

    Add base template for user_profile view

--- src/users/views.py
+++ src/users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import login
-from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 
 from .forms import CustomUserCreationForm
@@ -8,6 +10,10 @@ from .forms import CustomUserCreationForm
 def dashboard(request):
     return render(request, "users/dashboard.html")
 
+@login_required
+def user_profile(request, username):
+    profile_user = get_object_or_404(User, username=username)
+    return render(request, 'users/user_profile.html', {'profile_user': profile_user})
 
 def sign_up(request):
     if request.method == "POST":
