1021f6654323a42cf946c14da93bc556dbfc8263
kingwhatthe <sabalschuster06@gmail.com>
2025-11-25 18:34:26 -0500

    fixed minor whitespace

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -4,7 +4,6 @@ from .views import create_post_view, post_view, toggle_like_view, toggle_attend_
 urlpatterns = [
     path('create/', create_post_view, name='create_post'),
     path('<int:id>/', post_view, name='posts'),
-    path('<int:id>/like/', toggle_like_view, name='post-like')
-    ,
+    path('<int:id>/like/', toggle_like_view, name='post-like'),
     path('<int:id>/attend/', toggle_attend_view, name='post-attend')
 ]
